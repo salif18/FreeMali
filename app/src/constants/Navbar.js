1ffbc42d5a6bfc83257021d9579cef64e7bb85bc
Your Name <you@example.com>
2023-06-10 00:26:38 +0000

    changement de navbar

--- app/src/constants/Navbar.js
+++ app/src/constants/Navbar.js
@@ -6,7 +6,10 @@ import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import SettingsPowerIcon from '@mui/icons-material/SettingsPower';
 import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
-
+import HomeIcon from '@mui/icons-material/Home';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import EngineeringIcon from '@mui/icons-material/Engineering';
+import ContactlessIcon from '@mui/icons-material/Contactless';
 const Navbar = () => {
  const {myProfile,logout,isInLine, defaultImage ,newOffre} = useContext(MyStore)
 
@@ -19,10 +22,10 @@ const Navbar = () => {
 
             <div className='navbar-left' >
              <h2 className='logo-title'>FreeMali</h2>
-             <NavLink className={({isActive})=> isActive ? 'active':'navbar-left'} to='/'>Accueil</NavLink> 
-             <NavLink className={({isActive})=> isActive ? 'active':''} to='/blogs'>Prestataires</NavLink>
-             <NavLink className={({isActive})=> isActive ? 'active':''} to='/offres'><div>Offres d'emploi<div className='badge'>{newOffre >0  && <span>{newOffre }</span>}</div></div></NavLink>
-             <NavLink className={({isActive})=> isActive ? 'active':''} to='/contacts'>Contacts</NavLink>
+             <NavLink className={({isActive})=> isActive ? 'active':''} to='/'><HomeIcon style={{fontSize:29}}/>Accueil</NavLink> 
+             <NavLink className={({isActive})=> isActive ? 'active':''} to='/blogs'><EngineeringIcon style={{fontSize:29}} />Prestataires</NavLink>
+             <NavLink className={({isActive})=> isActive ? 'active':''} to='/offres'><BusinessCenterIcon style={{fontSize:29}} />Offres d'emploi</NavLink>
+             <NavLink className={({isActive})=> isActive ? 'active':''} to='/contacts'><ContactlessIcon style={{fontSize:29}} />Contacts</NavLink>
             </div>
             
             <div className='navbar-rigth'>
@@ -51,4 +54,4 @@ export default Navbar;
 // <NavLink ><FacebookSharpIcon style={{color:'rgba(0, 132, 255, 0.726)',fontSize:30,fontWeight:'bold'}} /></NavLink>
 //  <NavLink><TwitterIcon style={{color:'rgba(0, 132, 255, 0.726)',fontSize:30,fontWeight:'bold'}} /></NavLink>
 //  <NavLink><InstagramIcon style={{color:'#f3086280',fontSize:30,fontWeight:'bold'}} /></NavLink><span className='len-offre'>{offres.length}</span>
-// 
+// <div className='badge'>{newOffre >0  && <span>{newOffre }</span>}</div>
